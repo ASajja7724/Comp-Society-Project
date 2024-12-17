0965e75697078b95e7cc6d0334eba31c8ed27d23
Samuel-OG24 <samueladeola2008@gmail.com>
2024-12-17 13:55:12 +0000

    Added check syntax function
    Added a function to compile the file to print the line that the error is on

--- Read_error_message.py
+++ Read_error_message.py
@@ -3,3 +3,14 @@ def read_error_msg():   # function to access terminal and print error message to
     pass
   except ValueError:
     print("There was a value error")
+
+def check_syntax(file_path):
+  try:
+    with open(file_path, "r") as file:
+      code = file.read()
+    compile(code, file_path, "exec")
+    print("No syntax errors found.")
+  except SyntaxError as error:
+    print(f"Syntax error found:\n File\"{error.filename}", line{error.lineno}\n{error.text.strip()}\n {'^':>{error.offset}}")
+
+check_syntax("README.md")
