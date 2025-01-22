a1433e450c402d2afc7e2aa4fdab5c38e301f71c
Samuel-OG24 <samueladeola2008@gmail.com>
2025-01-22 16:26:08 +0000

    Update Read_error_message.py
    Stored the error line in a variable and passed it to a check_keyword function

--- Read_error_message.py
+++ Read_error_message.py
@@ -1,16 +1,16 @@
-def read_error_msg():   # function to access terminal and print error message to file
-  try:
-    pass
-  except ValueError:
-    print("There was a value error")
-
-def check_syntax(file_path):
-  try:
+def check_syntax(file_path): # Function to run program and check for error message
+  try: # Tries to run code by compiling it
     with open(file_path, "r") as f:
       code = f.read()
     compile(code, file_path, "exec")
     print("No syntax errors found.")
-  except SyntaxError as e:
+  except SyntaxError as e: # Checks if they compilation returns an error value then saves the line with the error to a variable
     print(f"Syntax error found:\n File\"{e.filename}\", line{e.lineno}\n{e.text.strip()}\n ")
+    error = e.text.strip()
+  return error
 
-check_syntax("README.py")
+def check_keyword(error):
+  print(error)
+
+error_line = check_syntax("README.py")
+check_keyword(error_line)
